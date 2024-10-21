f5ef39926b49e10b013e2b70f25238aef6ff87ee
Patryk Pacocha <115879093+pakx2206@users.noreply.github.com>
2024-10-21 09:48:43 +0200

    21.10.2024 start of the project
    Basic GUI setup for an app, added few functions

---
+++ CSVConverter/README.md
@@ -0,0 +1,2 @@
+# CSVConverter
+ An easy app to convert CSV files written in Java
---
+++ src/main/java/csvconverter/patrykpacocha/csvconverter/CsvConverter.java
@@ -0,0 +1,16 @@
+package csvconverter.patrykpacocha.csvconverter;
+import javax.swing.JFrame.*;
+import javax.swing.JPanel.*;
+/**
+ *
+ * @author patryk
+ *
+ */
+public class CsvConverter {
+
+    public static void main(String[] args) {
+        Frame t = new Frame();
+        t.setVisible(true);
+        
+    }
+}
